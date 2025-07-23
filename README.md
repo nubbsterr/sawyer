b7f3d05cb9cc950d5ee1946df15a6ddaf534d0da
nubbsterr <sherm5344@gmail.com>
2025-07-23 15:45:55 -0400

    more ports, better logic overall, nicer output w/ tables

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # sawyer
 > [!CAUTION]
-> Port scanning should only be done in contained, lab environments or in penetration tests with written authorization within your rules of engagement. Running port scanners may trigger NDR (Network Detection and Response) systems and flag you as a malicious actor which will get you in trouble!
+> Port scanning should only be done in contained, lab environments or in penetration tests with written authorization within your rules of engagement. Running port scanners may trigger NDR (Network Detection and Response) systems and flag you as a malicious actor and make the blue teamers hate their day.
 
 My own multithreaded TCP/UDP port scanner written in Python for enumeration.
 
 # agenda
-Appears to work a bit thus far. Still needs some testing in lab environments to see if the scanning actually works lol. It runs really fast which is nice.
+Appears to work a bit thus far. Still needs some testing in lab environments to see if the scanning actually works lol. It runs really fast which is nice. I found that Futures runs waaaay faster than Threading, but I'm like certain it's cuz I was missing sumn w/ my scripting logic.
 
-I plan to add in a feature to specify extra ports to scan, so we can say check for WinRM on port 5985 without having to set the range all the way up there.
+# dependencies
+Make sure to have all needed dependencies. I used `pipreqs` to create the `requirements.txt` list but if anything is missing please DM me on Discord @ nubbieeee.
--- sawyer.py
+++ sawyer.py
@@ -1,13 +1,14 @@
-import socket, time, re, sys
+import socket, sys, subprocess
+from time import time
+from re import match
 from concurrent.futures import ThreadPoolExecutor
-# ping scan next? OS detection?
+from tabulate import tabulate 
 
-refusedports = []
-closedports = []
+openports = []
 
 def help():
-    print("[+] Usage: python3 sawyer.py <target IP> [starting port] [ending port] [--udp]")
-    print("[+] The '--udp' flag can be set to perform UDP scans. By default, TCP scans are performed.")
+    print("[+] Usage: python3 sawyer.py <target IP> [starting port] [ending port] [--udp] [--debug]")
+    print("[+] --udp: Perform UDP scans. By default, TCP scans are performed.")
     print("[!] If no starting or ending port is specified, port range 1-1024 will be used!")
     sys.exit(0)
 
@@ -26,17 +27,17 @@ def tcpScan(target, port):
     try:
         sock.connect((target, port))
         connected = true
-        print(f"[+] {target}:{port} is open and running {resolveService(port, "tcp")}")
+        openports.append([port, resolveService(port, "tcp")])
     except ConnectionRefusedError:
-        refusedports.append(port)
+        pass
     except Exception:
-        closedports.append(port)
+        pass
     finally:
         try:
             if connected:
                 sock.shutdown(socket.SHUT_RDWR)
         except OSError as err:
-            print(f"An error occured during shutdown: {err}")
+            print(f"[!] For port {port}, an error occured during connection shutdown: {err}")
         sock.close()
 
 def udpScan(target, port):
@@ -46,52 +47,66 @@ def udpScan(target, port):
         sock.connect((target, port))
         sock.sendto(b'Hello', (target, port))
         sock.recvfrom(1024) # listen for ICMP response
-        print(f"[+] {target}:{port} is open and running {resolveService(port, "udp")}")
+        openports.append([port, resolveService(port, "udp")])
     except ConnectionRefusedError:
-        refusedports.append(port)
+        pass
     except Exception:
-        closedports.append(port)
+        pass
     finally:
-        # no need to shutdown since UDP is connectionless; no connection to shutdown really
+        # no need to shutdown since UDP is connectionless
         sock.close()
+
+def isHostUp(target):
+    if subprocess.run(["ping", "-c", "1", target], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0:
+        print(f"[+] Host ({target}) is up!\n")
+    else:
+        print(f"[+] Host ({target}) is down. Check your network connections.)\n")
+        sys.exit(1)
 
 # create scanning threads and go
 def main(target, start, end, mode):
-    print("Sawyer v1.0.0 by nubb (nubbsterr). A multithreaded port scanner written in Python.")
-	print("[-] Refused ports are ports that refused a connection. They may be in an ignored state.")
-    start_time = time.time()
+    print("Sawyer v1.1.0 by nubb (nubbsterr). A multithreaded port scanner written in Python.")
+    print(f"[+] Scanning {target}. First confirming if host is live...\n")
+    isHostUp(target)
+
+    start_time = time()
     with ThreadPoolExecutor(max_workers=100) as executor:
         futures = []
-        for port in range(start, end + 1):
-            if mode == "udp":
-                futures.append(executor.submit(udpScan, target, port))
-            elif mode == "tcp":
-                futures.append(executor.submit(tcpScan, target, port))
+		if mode == "udp":
+			for port in range(start, end + 1):
+				futures.append(executor.submit(udpScan, target, port))
+			futures.append(executor.submit(udpScan, target, 2049))
+			futures.append(executor.submit(udpScan, target, 3389))
+			futures.append(executor.submit(udpScan, target, 5985))
+		if mode == "tcp":
+			for port in range(start, end + 1):
+				futures.append(executor.submit(tcpScan, target, port))
+			futures.append(executor.submit(tcpScan, target, 2049))
+			futures.append(executor.submit(tcpScan, target, 3389))
+			futures.append(executor.submit(tcpScan, target, 5985))
+
         # Wait for all tasks to complete
         for future in futures:
             future.result()
 
-    end_time = time.time()
-    closedports.sort()
-    refusedports.sort()
-    print(f"[+] Scan complete. Took {end_time - start_time} seconds. Scanned {(end - start) + 1} ports.")
-	input("[-] Show closed/refused ports? [Enter to continue, Crtl+C to exit] ")
-    print(f"[+] Closed ports: {closedports}")
-    print(f"[+] Refused ports: {refusedports}")
+    end_time = time()
+    print(f"[+] Scan complete. Took {end_time - start_time} seconds. Scanned {(end - start) + 1} ports.\n")
+    sortedopen = sorted(openports)
+    print(tabulate(sortedopen, headers=['Port', 'Service']))
 
 # verify that passed arguments are valid before scanning
 def args():
     if "help" in sys.argv:
         help()
 
-    if (len(sys.argv) < 2) or (not re.match(r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", sys.argv[1])):
+    if (len(sys.argv) < 2) or (not match(r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", sys.argv[1])):
         print("[x] Specify an IPv4 address! No domains or IPv6 trash pls and thx.")
         help()
     
     mode = "tcp"
     if "--udp" in sys.argv:
         mode = "udp"
-
+    
     start, end = 1, 1024
     # are all arguments supplied
     if len(sys.argv) >= 4:
