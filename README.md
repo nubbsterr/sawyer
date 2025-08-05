ba36def5aa97ba0156c6dc0562c50630d7652943
nubbsterr <sherm5344@gmail.com>
2025-08-05 12:06:34 -0400

    OS detection and domain resolution

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # sawyer
 > [!CAUTION]
-> Port scanning should only be done in contained, lab environments or in penetration tests with written authorization within your rules of engagement. Running port scanners may trigger NDR (Network Detection and Response) systems, NIDS/NIPS (Network Intrusion Detection/Prevemtion System), etc., and flag you as a malicious actor and make a SOC team or MSSP hate their lives cuz they have another incident to handle potentially.
+> Port scanning should only be done in contained, lab environments or in penetration tests with written authorization by a client. Running port scanners may trigger NDR (Network Detection and Response) systems and flag you as a malicious actor.
 
 My own multithreaded TCP/UDP port scanner written in Python for enumeration.
 
 # agenda
-Actually works, at least when scanning against localhost, it shows ssh and smtp open lol. Also added coloured terminal output using ANSI escape codes, so no dependencies needed, which is great.
-
-Next thing to try is getting more telemetry on the scanned machine, like greater service information and OS information.
+Domain resolution is now possible thanks to the socket library being goated. Also OS detection, and added 8080 as an extra port to scan for. Overall the scanner is basically complete with how many features there are imo.
 
 # dependencies
 No external packages are needed for running this scanner, according to `pipreqs`. DM me on Discord @ nubbieeee if you have questions tho.
--- sawyer.py
+++ sawyer.py
@@ -1,6 +1,6 @@
 import socket, sys, subprocess, threading
 from time import time
-from re import match
+from re import match, search
 from concurrent.futures import ThreadPoolExecutor
 # tabulate build broke so none of that lol
 
@@ -15,7 +15,7 @@ refusedports_lock = threading.Lock()
 RED = '\033[31m'
 GREEN = '\033[32m'
 RESET = '\033[0m'
-ORANGE = '\033[33m'
+YELLOW = '\033[33m'
 #BLUE = '\033[34m'
 
 def help():
@@ -30,7 +30,7 @@ def resolveService(port, protocol):
         service = socket.getservbyport(port, protocol)
         return service
     except Exception:
-        print(f"{ORANGE}[-] An error occurred while attempting to resolve port {port}'s service.{RESET}")
+        print(f"{YELLOW}[-] An error occurred while attempting to resolve port {port}'s service.{RESET}")
         return "Unknown service"
 
 def tcpScan(target, port):
@@ -70,21 +70,51 @@ def udpScan(target, port):
         # no need to shutdown since UDP is connectionless
         sock.close()
 
+def OSdetection(target, pingout):
+# get TTL from ping output to get very lazy and experimental OS detection
+    print(f"{YELLOW}[!] OS detection is based on TTL. This is totally reliant on TTL not being altered by other network devices or system configurations.")
+    ttlmatch = search(r"ttl=(\d+)", pingout.lower())
+    ttl = int(ttlmatch.group(1))
+
+    if ttl <= 64:
+        return "Linux/Unix"
+    elif ttl <= 128:
+        return "Windows"
+    elif ttl <= 255:
+        return "Solaris/Unix"
+
+    return f"Unknown Operating system, TTL: {ttl}"
+
 def isHostUp(target):
-    if subprocess.run(["ping", "-c", "1", target], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0:
-        print(f"{GREEN}[+] Host ({target}) is up!{RESET}\n")
+    result = subprocess.run(["ping", "-c", "1", target], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
+    if result.returncode == 0:    
+        print(f"{GREEN}[+] Host ({target}) is up!{RESET}")
+        print(f"{GREEN}[+] Operating system is: {OSdetection(target, result.stdout)}{RESET}\n")
     else:
-        print(f"{RED}[x] Host ({target}) is down. Check your network connections.){RESET}\n")
+        print(f"{RED}[x] Host ({target}) is down. Check your network/VPN connections.){RESET}\n")
+        sys.exit(1)
+
+def resolveTarget(target):
+    ip_match = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    if match(target, ip_match):
+        return target # an IPv4 address
+
+    try:
+        ipv4 = socket.gethostbyname(target)
+        print(f"{GREEN}[+] Resolved {target} to {ipv4}.{RESET}")
+        return ipv4
+    except socket.gaierror:
+        print(f"{RED}[x] Could not resolve {target}. Check the hostname in your /etc/hosts file, or other network connections.{RESET}")
         sys.exit(1)
 
 # create scanning threads and go
 def main(target, start, end, mode):
-    print(f"\n{GREEN}[+] Sawyer v2.1.1 by nubb (nubbsterr). A multithreaded port scanner written in Python.{RESET}")
+    print(f"\n{GREEN}[+] Sawyer v2.2.0 by nubb (nubbsterr). A multithreaded port scanner written in Python.{RESET}")
     print(f"{GREEN}[+] Scanning {target}. First confirming if host is live...{RESET}")
     isHostUp(target)
 
     start_time = time()
-    extraports = [2049, 3389, 5985] # NFS, RDP, WinRM
+    extraports = [2049, 3389, 5985, 8080] # NFS, RDP, WinRM, HTTP-alt
     ports = list(range(start, end + 1)) + [port for port in extraports if (port < start) or (port  > end)]
 
     workers = min(100, max(10, len(ports) // 20)) # max of 100 workers or min of 10, adjust to port range
@@ -109,20 +139,22 @@ def main(target, start, end, mode):
     sortedrefused = sorted(refusedports)
     # very iffy but it works lol, tabulate broken
     print(f"{GREEN}Open ports:{RESET}")
-    for service in sortedopen:
-        print(service)
+    for port, service in sortedopen:
+        print(f"Port {port}: {service}")
     print(f"\n{RED}Refused/filtered ports:{RESET}")
-    for service in sortedrefused:
-        print(service)
+    for port, service in sortedrefused:
+        print(f"Port {port}: {service}")
 
 # verify that passed arguments are valid before scanning
 def args():
     if "help" in sys.argv:
         help()
 
-    if (len(sys.argv) < 2) or (not match(r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", sys.argv[1])):
-        print(f"{RED}[x] Specify an IPv4 address! No domains or IPv6 trash pls and thx.{RESET}")
+    if (len(sys.argv) < 2):
+        print(f"{RED}[x] Specify an IPv4 address or domain name!{RESET}")
         help()
+    
+    target = resolveTarget(sys.argv[1])
     
     mode = "tcp"
     if "--udp" in sys.argv:
@@ -135,14 +167,14 @@ def args():
             start = int(sys.argv[2])
             end = int(sys.argv[3])
             if not (1 <= start <= end <= 65535):
-                print(f"{ORANGE}[-] Invalid port range. Port range must be between 1 and 65535. Using default range 1-1024.{RESET}")
+                print(f"{YELLOW}[-] Invalid port range. Port range must be between 1 and 65535. Using default range 1-1024.{RESET}")
                 start, end = 1, 1024
         except Exception as err:
-            print(f"{ORANGE}[-] An exeception occurred when evaluating port range. Using default range 1-1024. Error: {RESET}{err}")
+            print(f"{YELLOW}[-] An exeception occurred when evaluating port range. Using default range 1-1024. Error: {RESET}{err}")
             start, end = 1, 1024
     else:
-        print(f"{ORANGE}[-] No port range was given. Using default range 1-1024.{RESET}")
+        print(f"{YELLOW}[-] No port range was given. Using default range 1-1024.{RESET}")
         start, end = 1, 1024
-    main(sys.argv[1], start, end, mode)
+    main(target, start, end, mode)
 
 args()
